546ace0d3791098a1e0c150538b46a7d0add78f4
Lunacywonders <Isaacn9394@gmail.com>
2025-09-22 09:32:04 -0700

    dsg

---
+++ app.js
@@ -0,0 +1,47 @@
+// // //let allows the variable to change
+// // //let x = 5;
+
+// // //x = 6;
+// // //const cannot be redefined
+// // // const x = 80;
+// // // console.log(x);
+// // // Booleans represent true and false
+// // const citizen  = true;
+// // //string represent names, characters etc.
+// // const name = "Jonoth";
+// // // integers/ numbers
+// // const age = 17;
+// // //null is the absence of value
+// // let f = null;
+// // //undefined
+// // let g;
+// // // functions are input output machines that can accept arguments or perameters
+// // //declare and defined the function
+// // // function add(x,y){
+// // // // console.log ( x + y );
+// // // return x + y;
+// // // }
+// // // // run the function
+// // // const bill = add (5,15);
+// // // console.log(bill);
+
+// // //scope
+
+// // // const x = 7;
+// // function helloX() {
+// //     //scopes variables are innaccessible outside of their function
+// //     const x = 9;
+// //     //console.log (x);
+// // }
+// // console.log(x);
+
+// const name = "Ben";
+// //template literal allows us to use variables inside our strings
+// console.log ('Hello ${name}')
+function teamrocket() {
+    let p = Math.floor(Math.random() * 101);
+    let b = Math.floor(Math.random() * 101);
+    let d = Math.floor(Math.random() * 101);
+    return p, b, d;
+}
+console.log (p, b, d);
