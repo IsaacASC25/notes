365499262f80f94f82fc094dcfbfe85fb6f94448
Lunacywonders <Isaacn9394@gmail.com>
2025-09-29 09:23:56 -0700

    qwrf

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const students = ["Jean", "London", "Damian", "Thomas"];
+students.push ("Isaac");
+console.log (students);
 /* console.log (students[1]) */
 
 /* function printAll(list){
@@ -19,9 +21,16 @@ const add2 = (a,b) => a + b;
 console.log (add2(5,6));
  */
 /* const found = students.find ((student) => student === "london");
-console.log(found); */
-const numbs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+/* console.log(found); */
+/* const numbs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // const x = numbs.filter((numb) => numb % 2 === 0);
 // console.log(x);
 numbs.filter ((numb) => numb % 2 === 0).forEach((el) => console.log (el));
 
+ */ 
+let answer = "";
+while (answer) {
+    console.log ("Countdown: " + number);
+    number--;
+}
+console.log ("Blast off!")
