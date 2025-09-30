54298961399d2223fcc06578f94ad005041e0587
Lunacywonders <Isaacn9394@gmail.com>
2025-09-30 09:34:22 -0700

    assfd

---
+++ appp.js
@@ -0,0 +1,18 @@
+const sentence = "The red cat sat on the mat. Why are you so sad cat? Don't ask that."
+function lang(sentence) {
+    let t = 0;
+    let s = 0;
+    for (let i = 0; i < sentence.length; i++){
+        if (sentence[i] === "S" || sentence [i] === "s") {
+            s++
+        } else if (sentence[i] === "T" || sentence [i] === "t") {
+            t++
+        }
+    }
+    if (t>s){
+    console.log ("english");
+    } else {
+        console.log ("Parle Vu Francaise")
+    }
+}
+lang (sentence)
